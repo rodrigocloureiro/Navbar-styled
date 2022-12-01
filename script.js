c9c806ef679faa94b6ffdefc8c1a4c1643001e1a
Rodrigo Loureiro <110643385+rodrigocloureiro@users.noreply.github.com>
2022-12-01 11:47:22 -0300

    version 2
    Navbar styled with CSS & pure JavaScript

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 const circle = document.querySelector('#circuloSobreicon');
 const indicator = document.querySelector('.indicator');
+const indicador = document.querySelector('#indicador');
 
 function slider(el) {
     const item = el.getAttribute('id');
+    const svg = el.children[0];
+    const svgs = document.querySelectorAll('svg');
+    svgs.forEach(item => item.id = '');
+      
     if(item === 'about') {
         circle.className = 'slide-one';
-        indicator.className = 'indicator indicator-one';
+        // indicator.className = 'indicator indicator-one';
+        indicador.className = 'indicator-one';
+        svg.id = 'selected';
     } else if(item === 'services') {
         circle.className = 'slide-two';
-        indicator.className = 'indicator indicator-two';
+        // indicator.className = 'indicator indicator-two';
+        indicador.className = 'indicator-two';
+        svg.id = 'selected';
     } else if(item === 'projects') {
         circle.className = 'slide-three';
-        indicator.className = 'indicator indicator-three';
+        // indicator.className = 'indicator indicator-three';
+        indicador.className = 'indicator-three';
+        svg.id = 'selected';
     } else {
         circle.className = 'slide-default';
-        indicator.className = 'indicator indicator-default';
+        // indicator.className = 'indicator indicator-default';
+        indicador.className = 'indicator-default';
+        svg.id = 'selected';
     }
 }
